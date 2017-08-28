ad47649e9694ef144ba7c3a2740b3b42722f99ef
James Lambie <jameslambie@gmail.com>
2017-08-28 18:25:18 +0100

    test: add test suite

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const RSMQ = require('rsmq')
 
 /*
@@ -6,98 +8,123 @@ const RSMQ = require('rsmq')
  * respects message signatures that should be deferred until
  * a later time (specified in config)
  */
-module.exports = function Queue (opts) {
-  // instantiate message queue
-  const rsmq = new RSMQ({
-    host: opts.host,
-    port: opts.port
-  })
-  
+let QueueWrapper = function (options) {
+  this.options = options
+
+  // initialise queue
+  this.rsmq = this.initialiseQueue()
+
   // initialise connection state
-  var connected = null
-  rsmq.on('connect', () => connected = true)
-  rsmq.on('disconnect', () => connected = false)
-  
-  // do request, fail or wait
-  function request (req, err) {
-    if (connected === true) return req()
-    if (connected === false) return err()
+  this.connected = null
 
-    rsmq.on('connect', connect)
-    rsmq.on('disconnect', disconnect)
-    
-    function connect () {
-      req()
-      removeListeners()
-    }
-    
-    function disconnect () {
-      err()
-      removeListeners()
-    }
-    
-    function removeListeners () {
-      rsmq.removeListener('connect', connect)
-      rsmq.removeListener('disconnect', disconnect)
-    }
-  }
-  
-  // public send function
-  this.send = function (message, done) {
-    function send () {
-      var options = {
-        qname: opts.name,
-        message: message,
-        delay: getDelay(message)
-      }
-      rsmq.sendMessage(options, done)
-    }
+  this.rsmq.on('connect', () => {
+    this.connected = true
+  })
 
-    function error () {
-      done(new Error('Queue server connection refused'))
-    }
+  this.rsmq.on('disconnect', () => {
+    this.connected = false
+  })
 
-    request(send, error)
-  }
-
-  // determine message delay (or 0)
-  function getDelay (message) {
-    return isDeferred(message)
-      ? untilStart() / 1000
-      : 0
-  }
-
-  // is message signature in deferred list?
-  function isDeferred (message) {
-    if (!opts.deferred) return false
-    return opts.deferred.messages.some((value) => {
-      return message.startsWith(value)
-    })
-  }
-  
-  // how long until deferred message window?
-  function untilStart () {
-    var now = new Date()
-    var start = parseTime(opts.deferred.start)
-    var stop = parseTime(opts.deferred.stop)
-    
-    if (now >= start) { // in or later than window
-      if (now < stop || stop < start) return 0 // in window or rollover
-      return 24 * 60 * 60 * 1000 - (now - start) // later than window
-    }
-    if (now < start) { // in or before window
-      if (now < stop && stop < start) return 0 // in window or rollover
-      return start - now // earlier than window
-    }
-  }
-  
-  // parse time string from config file
-  function parseTime (string) {
-    var time = new Date()
-    var [hrs, mins] = string.split(':')
-    time.setUTCHours(hrs, mins)
-    return time
-  }
-    
   return this
 }
+
+// instantiate message queue
+QueueWrapper.prototype.initialiseQueue = function () {
+  return new RSMQ({
+    host: this.options.host,
+    port: this.options.port
+  })
+}
+
+// public send function
+QueueWrapper.prototype.send = function (message, done) {
+  const send = () => {
+    let options = {
+      qname: this.options.name,
+      message: message,
+      delay: this.getDelay(message)
+    }
+
+    this.rsmq.sendMessage(options, done)
+  }
+
+  function error () {
+    done(new Error('Queue server connection refused'))
+  }
+
+  this.request(send, error)
+}
+
+// do request, fail or wait
+QueueWrapper.prototype.request = function (req, err) {
+  if (this.connected === true) {
+    return req()
+  }
+
+  if (this.connected === false) {
+    return err()
+  }
+
+  function connect () {
+    req()
+    removeListeners()
+  }
+
+  function disconnect () {
+    err()
+    removeListeners()
+  }
+
+  function removeListeners () {
+    this.rsmq.removeListener('connect', connect)
+    this.rsmq.removeListener('disconnect', disconnect)
+  }
+
+  this.rsmq.on('connect', connect)
+  this.rsmq.on('disconnect', disconnect)
+}
+
+// determine message delay (or 0)
+QueueWrapper.prototype.getDelay = function (message) {
+  return this.isDeferred(message)
+    ? this.untilStart() / 1000
+    : 0
+}
+
+// is message signature in deferred list?
+QueueWrapper.prototype.isDeferred = function (message) {
+  if (!this.options.deferred) return false
+
+  if (!Array.isArray(this.options.deferred.messages)) return false
+
+  return this.options.deferred.messages.some((value) => {
+    return message.startsWith(value)
+  })
+}
+
+// how long until deferred message window?
+QueueWrapper.prototype.untilStart = function () {
+  const now = new Date()
+  const start = this.parseTime(this.options.deferred.start)
+  const stop = this.parseTime(this.options.deferred.stop)
+
+  if (now >= start) { // in or later than window
+    if (now < stop || stop < start) return 0 // in window or rollover
+    return 24 * 60 * 60 * 1000 - (now - start) // later than window
+  }
+
+  if (now < start) { // in or before window
+    if (now < stop && stop < start) return 0 // in window or rollover
+    return start - now // earlier than window
+  }
+}
+
+// parse time string from config file
+QueueWrapper.prototype.parseTime = function (string) {
+  const time = new Date()
+  const timeParts = string.split(':')
+  time.setUTCHours(timeParts[0], timeParts[1])
+  return time
+}
+
+module.exports = QueueWrapper
